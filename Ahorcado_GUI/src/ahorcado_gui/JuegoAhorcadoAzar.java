0e1733a881c397c13d5160e8e94376876c708a50
Alejandr0-Ramirez <alejoramseg@gmail.com>
2026-02-13 09:34:17 -0600

    Clase azar corregido

--- Ahorcado_GUI/src/ahorcado_gui/JuegoAhorcadoAzar.java
+++ Ahorcado_GUI/src/ahorcado_gui/JuegoAhorcadoAzar.java
@@ -1,13 +1,45 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ahorcado_gui;
 
-/**
- *
- * @author Alejandro R
- */
-public class JuegoAhorcadoAzar {
-    
+import java.util.Random;
+
+public class JuegoAhorcadoAzar extends JuegoAhorcadoBase {
+
+    private AdminPalabrasSecretas admin;
+
+    public JuegoAhorcadoAzar(AdminPalabrasSecretas admin) {
+        super();
+        this.admin = admin;
+        inicializarPalabraSecreta();
+    }
+
+    @Override
+    public void inicializarPalabraSecreta() {
+        palabraSecreta = admin.obtenerPalabraAzar().toUpperCase();
+        palabraActual = "_".repeat(palabraSecreta.length());
+    }
+
+    @Override
+    public boolean verificarLetra(char letra) {
+        return palabraSecreta.indexOf(Character.toUpperCase(letra)) >= 0;
+    }
+
+    @Override
+    public void actualizarPalabraActual(char letra) {
+        char[] actual = palabraActual.toCharArray();
+        for (int i = 0; i < palabraSecreta.length(); i++) {
+            if (palabraSecreta.charAt(i) == Character.toUpperCase(letra)) {
+                actual[i] = Character.toUpperCase(letra);
+            }
+        }
+        palabraActual = new String(actual);
+    }
+
+    @Override
+    public boolean hasGanado() {
+        return palabraActual.equals(palabraSecreta);
+    }
+
+    @Override
+    public void jugar() {
+    }
 }
