0d70f56369d61501895a766e01ef1cdffee6fa30
Andrés Díaz <andresalbertodiazrevelo@gmail.com>
2026-02-13 09:49:47 -0600

    Codigo

--- Ahorcado_GUI/src/ahorcado_gui/JuegoAhorcadoFijo.java
+++ Ahorcado_GUI/src/ahorcado_gui/JuegoAhorcadoFijo.java
@@ -4,10 +4,45 @@
  */
 package ahorcado_gui;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author user
  */
-public class JuegoAhorcadoFijo {
+public class JuegoAhorcadoFijo extends JuegoAhorcadoBase {
+    JuegoAhorcadoFijo (String palabraSecreta ){
+        super();
+        this.palabraSecreta = palabraSecreta.toLowerCase();
+        inicializarPalabraSecreta();
+    }
+        
+        public void inicializarPalabraSecreta(){
+            iniciarGuiones();
+        }
+        
+public void actualizarPalabraActual(char letra){
+letra = Character.toUpperCase(letra);
+char [] caracteres = palabraActual.toCharArray();
+
+for (int i = 0;  i < palabraSecreta.length(); i++){
+    if (palabraSecreta.charAt(i) == letra){
+        caracteres[i] = letra;
+        
+    }
+    this.palabraActual = new String (caracteres); 
+}
+}
+
+public boolean verificarLetra (char letra){
+    letra = Character.toUpperCase(letra);
+    return palabraSecreta.indexOf(letra) >=0;
     
 }
+
+public boolean hasGanado(){
+    return palabraActual.equals(palabraSecreta);
+}
+
+}
+
