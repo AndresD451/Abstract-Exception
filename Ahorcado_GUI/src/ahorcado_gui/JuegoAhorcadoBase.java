e5d31d1a9c3f75035986155ef851089bbe242d2c
herminiosauceda0314 <micuentadosparami@gmail.com>
2026-02-13 08:58:44 -0600

    Clase JuegoAhorcadoBase

--- Ahorcado_GUI/src/ahorcado_gui/JuegoAhorcadoBase.java
+++ Ahorcado_GUI/src/ahorcado_gui/JuegoAhorcadoBase.java
@@ -4,10 +4,43 @@
  */
 package ahorcado_gui;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author user
  */
-public class JuegoAhorcadoBase implements JuegoAhorcado {
+public abstract class JuegoAhorcadoBase implements JuegoAhorcado {
     
+    protected String palabraSecreta;
+    protected String palabraActual;
+    protected int intentos;
+    protected int limiteIntentos = 6;
+    protected ArrayList<Character> letrasUsadas;
+    protected ArrayList<String> figuraAhorcado;
+    
+    public JuegoAhorcadoBase(){
+        this.letrasUsadas = new ArrayList<>();
+        this.figuraAhorcado = new ArrayList<>();
+        this.intentos = limiteIntentos;
+        
+    }
+    
+    public abstract void actualizarPalabraActual(char letra);
+    public abstract boolean verificarLetra(char letra);
+    public abstract boolean hasGanado();
+    
+    private void preperarFiguras(){
+        figuraAhorcado.add("  +---+\n  |   |\n      |\n      |\n      |\n      |\n========="); // 0 errores
+        figuraAhorcado.add("  +---+\n  |   |\n  O   |\n      |\n      |\n      |\n========="); // 1 error (cabeza)
+        figuraAhorcado.add("  +---+\n  |   |\n  O   |\n  |   |\n      |\n      |\n========="); // 2 errores (tronco)
+        figuraAhorcado.add("  +---+\n  |   |\n  O   |\n /|   |\n      |\n      |\n========="); // 3 errores (brazo izq)
+        figuraAhorcado.add("  +---+\n  |   |\n  O   |\n /|\\  |\n      |\n      |\n========="); // 4 errores (brazo der)
+        figuraAhorcado.add("  +---+\n  |   |\n  O   |\n /|\\  |\n /    |\n      |\n========="); // 5 errores (pierna izq)
+        figuraAhorcado.add("  +---+\n  |   |\n  O   |\n /|\\  |\n / \\  |\n      |\n========="); // 6 errores (pierna der)
+    }
+    
+    protected boolean esLetraRpetida(char letra){
+        return letrasUsadas.contains(Character.toLowerCase(letra));
+    }
 }
